836d65bad4d8524969f5cf78fecbe48526eb64e3
Gao Hongyuan <gao0927@gmail.com>
2015-07-17 14:13:07 +0800

    add countdown timer

--- app/src/main/java/ninja/hongyuan/podotimer/MainActivity.java
+++ app/src/main/java/ninja/hongyuan/podotimer/MainActivity.java
@@ -2,16 +2,49 @@ package ninja.hongyuan.podotimer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextClock;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
+
+    private TextClock mTextClock;
+    private TextView mTextField;
+    private CountDownTimer counter;
+    private boolean isStart;
+    private Button startButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // clock test
+        mTextClock = (TextClock)findViewById(R.id.textClock);
+        mTextClock.setFormat24Hour("yyyy-MM-dd hh:mm:ss");
+
+        // countdown test
+        mTextField = (TextView)findViewById(R.id.textview);
+        counter = new CountDownTimer(30000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                mTextField.setText("seconds remaining: " + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                mTextField.setText("done!");
+            }
+        };
+
+        isStart = false;
+        startButton = (Button)findViewById(R.id.toggle_button);
+
     }
 
     @Override
@@ -34,5 +67,21 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void toggle(View view) {
+        if (isStart) {
+            counter.cancel();
+            startButton.setText("Start");
+            mTextField.setText("Stopped");
+            isStart = false;
+        }
+
+        else {
+            counter.start();
+            startButton.setText("Cancel");
+            isStart = true;
+        }
+
     }
 }
